76ad37ec41d5881c15ccec434661fe8711399000
carolina_tapia <cstapiarubio@gmail.com>
2020-03-25 23:19:17 -0300

    consulta que devuelve todos los amigos de kermit

--- consulta_friendships.sql
+++ consulta_friendships.sql
@@ -1,6 +1,14 @@
-select users.first_name, users.last_name, users2.first_name as friend_nombre, users2.last_name as friend_apellido
+/*select users.first_name, users.last_name, users2.first_name as friend_nombre, users2.last_name as friend_apellido
 from users
 inner join friendships
 on users.id = friendships.user_id
 inner join users as users2 
 on friend_id = users2.id;
+
+select users.first_name, users.last_name
+from users
+inner join friendships
+on users.id = friendships.user_id
+inner join users as users2 
+on friend_id = users2.id
+where friend_id =4;*/
