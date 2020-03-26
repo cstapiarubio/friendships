830a4815ede905d4ad6587307e26740e2bbe5dc4
carolina_tapia <cstapiarubio@gmail.com>
2020-03-25 23:47:15 -0300

    nuevo usuario con amistades

--- consulta_friendships.sql
+++ consulta_friendships.sql
@@ -16,10 +16,24 @@ where friend_id =4;
 select count(friendships.id) as amistades
 from users
 inner join friendships
-on users.id = friendships.user_id;*/
+on users.id = friendships.user_id;
 
 select users.first_name, users.last_name, count(friendships.id) as amigos
 from users
 inner join friendships
 on users.id = friendships.user_id
 group by users.id;
+
+insert into users(first_name, last_name, created_at, updated_at)
+values("Bastian", "Afel", now(), now());
+
+insert into friendships(user_id, friend_id, created_at, updated_at)
+values (6, 2, now(), null);
+
+insert into friendships(user_id, friend_id, created_at, updated_at)
+values (6, 4, now(), null);
+
+insert into friendships(user_id, friend_id, created_at, updated_at)
+values (6, 5, now(), null);*/
+
+select * from friendships;
