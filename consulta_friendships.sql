cdee7aee9e2fd0c6621a7621b6bcc96a14acb635
carolina_tapia <cstapiarubio@gmail.com>
2020-03-25 23:13:52 -0300

    consulta de lista de usuarios junto con los nombres de sus amigos

--- consulta_friendships.sql
+++ consulta_friendships.sql
@@ -1,2 +1,6 @@
-select * 
-from friendships; 
+select users.first_name, users.last_name, users2.first_name as friend_nombre, users2.last_name as friend_apellido
+from users
+inner join friendships
+on users.id = friendships.user_id
+inner join users as users2 
+on friend_id = users2.id;
